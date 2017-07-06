b0422a041414a81d72e15916cb9e749ab0f2de0c
Paresh Salunke <ParryPC@Pareshs-MBP.home>
2017-07-05 21:43:14 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Parry.
+ */
+import _ from 'Lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyB3mZs1kQ5p72Sb3dLiLGTbtbWomNGFFeE';
+
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('matt stefanina');
+
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+		return(
+			<div>
+				<SearchBar onSearchTermChange= {videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect = {selectedVideo => this.setState({ selectedVideo })}
+					videos = {this.state.videos} />
+			</div>
+		)
+	}
+};
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
